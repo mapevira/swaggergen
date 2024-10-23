865ff1e8584e4c5ea4f7802a5ecaba645ee29158
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-10-23 12:03:03 +0200

    First Version

---
+++ src/main/java/com/mapfre/tron/api/swaggergen/SwaggergenApplication.java
@@ -0,0 +1,13 @@
+package com.mapfre.tron.api.swaggergen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggergenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggergenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mapfre/tron/api/swaggergen/SwaggergenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapfre.tron.api.swaggergen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggergenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
